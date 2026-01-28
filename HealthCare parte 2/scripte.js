cb5a0b71561268e8656d99cd5bc577f03ac776dd
l9iirch-hub <pchossam11@gmail.com>
2026-01-28 11:48:35 +0100

    modification

--- HealthCare parte 2/scripte.js
+++ HealthCare parte 2/scripte.js
@@ -1,6 +1,7 @@
 const form = document.getElementById("DemandeForm");
 const tablebody = document.getElementById("tableBody");
 const message = document.getElementById("message");
+const searchInput = document.getElementById("site-search");
 
 let demandes = [];
 
@@ -25,40 +26,60 @@ form.addEventListener("submit", function (e) {
     telephone,
     email,
     motif,
-    date
+    date,
   };
 
   demandes.push(demande);
-
-  const row = document.createElement("tr");
-  row.innerHTML = `
-    <td>${demande.nom}</td>
-    <td>${demande.prenom}</td>
-    <td>${demande.telephone}</td>
-    <td>${demande.email}</td>
-    <td>${demande.motif}</td>
-    <td>${demande.date}</td>
-    <td>
-      <button onclick="deleteRow(this)"> Supprimer</button>
-    </td>
-  `;
-
-  tablebody.appendChild(row);
-
+  AfficherTable(demandes);
   showMessage(" Commande ajoutée avec succès ", "green");
   form.reset();
 });
+function AfficherTable(data) {
+  tablebody.innerHTML = "";
+
+  data.forEach((demande) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+      <td>${demande.nom}</td>
+      <td>${demande.prenom}</td>
+      <td>${demande.telephone}</td>
+      <td>${demande.email}</td>
+      <td>${demande.motif}</td>
+      <td>${demande.date}</td>
+      <td>
+        <button onclick="deleteRow(this)">Supprimer</button>
+      </td>
+    `;
+    tablebody.appendChild(row);
+  });
+}
+
 function showMessage(text, color) {
   message.textContent = text;
   message.style.color = color;
 
   setTimeout(() => {
     message.textContent = "";
-  }, 2560);
+  }, 2500);
 }
-
 
 function deleteRow(button) {
   const row = button.parentElement.parentElement;
   row.remove();
-};
+}
+function searchD(text) {
+  const value = text.toLowerCase();
+
+  const result = demandes.filter(
+    (d) =>
+      d.nom.toLowerCase().includes(value) ||
+      d.prenom.toLowerCase().includes(value) ||
+      d.email.toLowerCase().includes(value) ||
+      d.motif.toLowerCase().includes(value),
+  );
+
+  AfficherTable(result);
+}
+searchInput.addEventListener("input", function () {
+  searchD(this.value);
+});
